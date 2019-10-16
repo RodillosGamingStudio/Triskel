2c7429bc943941d02311fea2526816dd38077002
Astarhel <manuel.martinr222@gmail.com>
2019-10-17 00:31:37 +0200

    creditos colocados y comienzo de barra de progreso

--- src/states/credits.js
+++ src/states/credits.js
@@ -13,18 +13,18 @@ class credits extends Phaser.Scene {
 
         var B = JSON.parse(buttons);
         var makebutton = new makeButton();
-        var back_button = makebutton.setButton(this, 70, 680, 'Lbutton', B.back[game.language], 'menuScene');
+        var back_button = makebutton.setButton(this, 140, 850, 'Lbutton', B.back[game.language], 'menuScene');
         back_button.tint = 	0x808080;
 
         var C = JSON.parse(credits_text);
 
-        this.add.text(640, 70, C.title, { fontFamily: '"Roboto Condensed"', fontSize:'60px', color:'#e2b40b' }).setOrigin(0.5);
-        this.add.text(640, 140, C.subtitle[game.language], { fontFamily: '"Roboto Condensed"', fontSize:'36px', color:'#e2b40b' }).setOrigin(0.5);
+        this.add.text(800, 87.5, C.title, { fontFamily: '"Roboto Condensed"', fontSize:'66px', color:'#e2b40b' }).setOrigin(0.5);
+        this.add.text(800, 175, C.subtitle[game.language], { fontFamily: '"Roboto Condensed"', fontSize:'42px', color:'#e2b40b' }).setOrigin(0.5);
 
         for(var i = 0; i < C.roles[game.language].length; i++){
-            this.add.text((i+1)*320, 260, C.roles[game.language][i], { fontFamily: '"Roboto Condensed"', fontSize:'30px', color:'#e2b40b' }).setOrigin(0.5);
+            this.add.text((i+1)*400, 325, C.roles[game.language][i], { fontFamily: '"Roboto Condensed"', fontSize:'36px', color:'#e2b40b' }).setOrigin(0.5);
             for(var j = 0; j < C.names[i].length; j++){
-                this.add.text((i+1)*320, 260 + (j+1) * 100, C.names[i][j], { fontFamily: '"Roboto Condensed"', fontSize:'20px', color:'#e2b40b' }).setOrigin(0.5);
+                this.add.text((i+1)*400, 325 + (j+1) * 100, C.names[i][j], { fontFamily: '"Roboto Condensed"', fontSize:'26px', color:'#e2b40b' }).setOrigin(0.5);
             }
         }
 
--- src/states/preload.js
+++ src/states/preload.js
@@ -4,6 +4,17 @@ class preload extends Phaser.Scene {
     }
 
     preload() {
+
+        //MUSICAS
+        this.load.audio('musica1', 'assets/Sonidos/musica1.mp3');
+        this.load.audio('musica2', 'assets/Sonidos/musica2.mp3');
+        this.load.audio('musica3', 'assets/Sonidos/musica3.mp3');
+
+
+        //SONIDOS
+        this.load.audio('clickCarta', 'assets/Sonidos/clickCarta.mp3');
+        this.load.audio('clickCelda', 'assets/Sonidos/clickCelda.mp3');
+
         this.load.image('fondo', 'assets/tablero1.jpg');
         this.load.image('fondo2', 'assets/tablero2.jpg');
         this.load.image('carta', 'assets/carta.jpg');
@@ -24,36 +35,87 @@ class preload extends Phaser.Scene {
 
         //this.load.image('tablero', 'assets/tablero.png');
         this.load.image('celda', 'assets/celda.png');
-        
+
         this.load.image('menu', 'assets/Pantallas/Menu.png');
         this.load.image('mode', 'assets/Pantallas/Mode.png');
         this.load.image('Scrollbar', 'assets/Botones/Scrollbar.png');
         this.load.image('Settings', 'assets/Pantallas/Settings.png');
 
-        this.load.spritesheet('Bbutton', 'assets/Botones/Bbutton.png',  { frameWidth: 267, frameHeight: 86 });
-        this.load.spritesheet('Lbutton', 'assets/Botones/Lbutton.png',  { frameWidth: 138, frameHeight: 75 });
-        this.load.spritesheet('Sbutton', 'assets/Botones/Sbutton.png',  { frameWidth: 271, frameHeight: 250 });        
-    
-
-        //MUSICAS
-        this.load.audio('musica1', 'assets/Sonidos/musica1.mp3');
-        this.load.audio('musica2', 'assets/Sonidos/musica2.mp3');
-        this.load.audio('musica3', 'assets/Sonidos/musica3.mp3');
+        this.load.spritesheet('Bbutton', 'assets/Botones/Bbutton.png', { frameWidth: 267, frameHeight: 86 });
+        this.load.spritesheet('Lbutton', 'assets/Botones/Lbutton.png', { frameWidth: 138, frameHeight: 75 });
+        this.load.spritesheet('Sbutton', 'assets/Botones/Sbutton.png', { frameWidth: 271, frameHeight: 250 });
 
 
-        //SONIDOS
-        this.load.audio('clickCarta', 'assets/Sonidos/clickCarta.mp3');
-        this.load.audio('clickCelda', 'assets/Sonidos/clickCelda.mp3');
-        
+
+/*
+        var progressBar = this.add.graphics();
+        var progressBox = this.add.graphics();
+        progressBox.fillStyle(0x222222, 0.8);
+        progressBox.fillRect(240, 270, 320, 50);
+
+        var width = this.cameras.main.width;
+        var height = this.cameras.main.height;
+        var loadingText = this.make.text({
+            x: width / 2,
+            y: height / 2 - 50,
+            text: 'Loading...',
+            style: {
+                font: '20px monospace',
+                fill: '#ffffff'
+            }
+        });
+        loadingText.setOrigin(0.5, 0.5);
+
+        var percentText = this.make.text({
+            x: width / 2,
+            y: height / 2 - 5,
+            text: '0%',
+            style: {
+                font: '18px monospace',
+                fill: '#ffffff'
+            }
+        });
+        percentText.setOrigin(0.5, 0.5);
+
+        var assetText = this.make.text({
+            x: width / 2,
+            y: height / 2 + 50,
+            text: '',
+            style: {
+                font: '18px monospace',
+                fill: '#ffffff'
+            }
+        });
+
+        assetText.setOrigin(0.5, 0.5);
+
+        this.load.on('progress', function (value) {
+            percentText.setText(parseInt(value * 100) + '%');
+            progressBar.clear();
+            progressBar.fillStyle(0xffffff, 1);
+            progressBar.fillRect(250, 280, 300 * value, 30);
+        });
+
+        this.load.on('fileprogress', function (file) {
+            assetText.setText('Loading asset: ' + file.key);
+        });
+
+        this.load.on('complete', function () {
+            progressBar.destroy();
+            progressBox.destroy();
+            loadingText.destroy();
+            percentText.destroy();
+            assetText.destroy();
+        });*/
 
     }
     create() {
-        this.scene.start('menuScene');       
+        this.scene.start('menuScene');
 
     }
 
     update() {
-        
+
 
     }
 }
